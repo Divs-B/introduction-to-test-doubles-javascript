977d3a88883957bb0a09afc40fd3c4a323779c58
Divya <divyabhatt16@gmail.com>
2022-03-15 06:20:57 +0000

    One test file edited for extension task of Jest test parameterisation

--- src/bubble_tea_order_service.test.js
+++ src/bubble_tea_order_service.test.js
@@ -1,42 +1,80 @@
-const {createOrderRequest} = require('./bubble_tea_order_service');
-const bubbleTeaType = require('./bubble_tea_type');
-const bubbleTeaMessenger = require('./bubble_tea_messenger');
-jest.mock('./bubble_tea_messenger');
-jest.mock('./simple_logger');
+const { createOrderRequest } = require('./bubble_tea_order_service')
+const bubbleTeaType = require('./bubble_tea_type')
+const bubbleTeaMessenger = require('./bubble_tea_messenger')
+jest.mock('./bubble_tea_messenger')
+jest.mock('./simple_logger')
 
-let dummyPaymentDetails;
-
-beforeEach(() => {
-  dummyPaymentDetails = {
-    name: 'Some person',
-    address: '123 Some Street',
-    debitCard: {
-      digits: '123456',
-    },
-  };
-});
+beforeEach(() => {})
 
 afterEach(() => {
-  jest.clearAllMocks();
-});
+  jest.clearAllMocks()
+})
 
-test('test successful bubble tea order request', () => {
+describe('test successful bubble tea order request', () => {
   // Arrange
-  const bubbleTeaRequest = {
-    paymentDetails: dummyPaymentDetails,
-    bubbleTea: {
-      type: bubbleTeaType.MATCHAMILKTEA,
+  //dummy data
+  const dummyPaymentDetails = [
+    {
+      name: 'Some person1',
+      address: '123 Some Street',
+      debitCard: {
+        digits: '1234567'
+      }
     },
-  };
+    {
+      name: 'Some person2',
+      address: '123 Some Street',
+      debitCard: {
+        digits: '1234568'
+      }
+    },
+    {
+      name: 'Some person3',
+      address: '123 Some Street',
+      debitCard: {
+        digits: '1234569'
+      }
+    }
+  ]
 
-  // Act
-  const orderRequest = createOrderRequest(bubbleTeaRequest);
+  it.each([
+    [
+      {
+        paymentDetails: dummyPaymentDetails[0],
+        bubbleTea: {
+          type: bubbleTeaType.MATCHAMILKTEA
+        }
+      },
+      dummyPaymentDetails[0]
+    ],
+    [
+      {
+        paymentDetails: dummyPaymentDetails[1],
+        bubbleTea: {
+          type: bubbleTeaType.JASMINEMILKTEA
+        }
+      },
+      dummyPaymentDetails[1]
+    ],
+    [
+      {
+        paymentDetails: dummyPaymentDetails[2],
+        bubbleTea: {
+          type: bubbleTeaType.LYCHEEICETEA
+        }
+      },
+      dummyPaymentDetails[2]
+    ]
+  ])("when the input is '%s'", (input, output) => {
+    const orderRequest = createOrderRequest(input)
 
-  // Assert
-  expect(orderRequest.name).toBe(dummyPaymentDetails.name);
-  expect(orderRequest.digits).toBe(dummyPaymentDetails.debitCard.digits);
-  expect(bubbleTeaMessenger.sendBubbleTeaOrderRequestEmail)
-      .toHaveBeenCalledWith(orderRequest);
-  expect(bubbleTeaMessenger.sendBubbleTeaOrderRequestEmail)
-      .toHaveBeenCalledTimes(1);
-});
+    expect(orderRequest.name).toBe(output.name)
+    expect(orderRequest.digits).toBe(output.debitCard.digits)
+    expect(
+      bubbleTeaMessenger.sendBubbleTeaOrderRequestEmail
+    ).toHaveBeenCalledWith(orderRequest)
+    expect(
+      bubbleTeaMessenger.sendBubbleTeaOrderRequestEmail
+    ).toHaveBeenCalledTimes(1)
+  })
+})
